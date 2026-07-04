d3087b6905de6ad1669a9475c8e7638a0945e01e
Andrey Pugachev <avpugachev@yandex.ru>
2026-07-04 14:56:14 +0300

    Version_0_1

---
+++ opinions_app/dropbox.py
@@ -0,0 +1,58 @@
+import asyncio
+
+import aiohttp
+import json
+
+from . import app
+
+AUTH_HEADER = f'Bearer {app.config["DROPBOX_TOKEN"]}'
+UPLOAD_LINK = 'https://content.dropboxapi.com/2/files/upload'
+SHARING_LINK = ('https://api.dropboxapi.com/2/'
+                'sharing/create_shared_link_with_settings')
+
+
+async def async_upload_files_to_dropbox(images):
+    if images is not None:
+        tasks = []
+        async with aiohttp.ClientSession() as session:
+            for image in images:
+                tasks.append(
+                    asyncio.ensure_future(
+                        upload_file_and_get_url(session, image)
+                    )
+                )
+            urls = await asyncio.gather(*tasks)
+        return urls
+
+
+async def upload_file_and_get_url(session, image):
+    dropbox_args = json.dumps({
+        'autorename': True,
+        'mode': 'add',
+        'path': f'/{image.filename}',
+    })
+    async with session.post(
+        UPLOAD_LINK,
+        headers={
+            'Authorization': AUTH_HEADER,
+            'Content-Type': 'application/octet-stream',
+            'Dropbox-API-Arg': dropbox_args
+        },
+        data=image.read()
+    ) as response:
+        data = await response.json()
+        path = data['path_lower']
+    async with session.post(
+        SHARING_LINK,
+        headers={
+            'Authorization': AUTH_HEADER,
+            'Content-Type': 'application/json',
+        },
+        json={'path': path}
+    ) as response:
+        data = await response.json()
+        if 'url' not in data:
+            data = data['error']['shared_link_already_exists']['metadata']
+        url = data['url']
+        url = url.replace('&dl=0', '&raw=1')
+    return url
